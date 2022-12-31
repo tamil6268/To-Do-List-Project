64cad5d65ebee83a9b6c95f0fe6feef46d72de5a
TAMILARASAN S <111070542+tamil6268@users.noreply.github.com>
2022-12-31 17:57:05 +0530

    third commit

--- jstodoproject.js
+++ jstodoproject.js
@@ -3,10 +3,10 @@ const empty=document.getElementById('tbody');//for hide text
 const newScreen=document.createElement('div');
 newScreen.id='poptoadd';
 document.body.appendChild(newScreen);
+
 const popUpTab=document.getElementById('popTab');//for add task
 const add=document.getElementById('addplus');
 add.addEventListener('click',addTask);//by click it will blur the page
-// const addItem=document.getElementById('amAddIcon');
 function addTask(){
     newScreen.classList.add('active');
     //    empty.classList.add('active');//to hide the no items here front page
@@ -23,107 +23,136 @@ clickAdd[0].addEventListener('click',()=>{
     popUpTab.classList.remove('active');
     newScreen.classList.remove('active');
     empty.classList.add('active');
-    newCard();
 });
-const updateTask=[];
-console.log(updateTask);
-function newCard(){
-    const userContent=document.getElementById('placename').value;//getting the text from user
-    const newobj={
-        placeName: userContent,
-    }
-    updateTask.push(newobj); 
-    popUpTab.classList.remove('active');
-    newScreen.classList.remove('active'); 
-    const parent = document.createElement("div");//child element
-    parent.setAttribute("id","child");
-    //console.log(parent);
-    const title=document.createElement('div');//title
-    title.setAttribute("class","title");
-    //console.log(title);
-    const line=document.createElement('hr');
-    const inner=document.createElement('div');//for work show and markbutton
-    inner.setAttribute("id","inner");
-    // console.log(inner);
-    const innerText=document.createElement('div');
-    innerText.setAttribute("id","innertext");
-    // console.log(innerText);
-    const btnMarkDone=document.createElement('button');
-    btnMarkDone.setAttribute("class","btnActive");
-    btnMarkDone.innerText="Mark Done";
-    // console.log(btnMarkDone);
-    const icon=document.createElement('div');//for icons delete and add plus icon
-    icon.setAttribute("class","icon");
-    // console.log(icon);
-    const btnDelete=document.createElement('i');  
-    btnDelete.setAttribute("class","fa-solid fa-trash-can");  
-    btnDelete.setAttribute("id","amDeleteIcon");  
-    btnDelete.setAttribute("onclick","deleteTaskInsideItems()");
-    // console.log(btnDelete);
-    const btnAdd=document.createElement('i');  
-    btnAdd.setAttribute("class","fa-sharp fa-solid fa-circle-plus"); 
-    btnAdd.setAttribute("id","amAddIcon");
-    btnAdd.setAttribute("onclick","addTaskInsideItems()");
-    // console.log(btnAdd); 
-//appendingchild's toparent element
-    document.body.append(parent);
-    parent.appendChild(title);
-    parent.appendChild(line);
-    parent.appendChild(inner);
-    inner.appendChild(innerText);
-    inner.appendChild(btnMarkDone);
-    parent.appendChild(icon);
-    icon.appendChild(btnDelete);
-    icon.appendChild(btnAdd);   
-    parent.classList.add('active');
-    title.innerText=`${updateTask[updateTask.length-1].placeName}`;  
-}
-////Entering to tasks and items
-// const updateList=[];
+let container = document.getElementById("container");
 
-updateTask.forEach(newContent => {
-    const newContent=()=>{
-        const userWork=document.getElementById('work').value;
-        const newobj1={
-            work:userWork,
-        }
-        updateTask.push(newobj1);
-        const inner=document.getElementById('inner');
-        const btnMarkDone=document.createElement('button');
-        btnMarkDone.setAttribute("id","btnActive");
-        const innerText=document.createElement('div');
-        innerText.setAttribute("id","innertext");
-        inner.appendChild(innerText);
-        innerText.classList.add('show');
-        innerText.innerHTML=`${updateTask[updateTask.length-1].work} <button id="btnActive">Mark Done</button>`;
-        const btnMark=document.getElementById('btnActive');
-        btnMark.addEventListener('click',(res)=>{
-                innerText.innerText=updateList[updateList.length-1].work;
-                innerText.style.color="brown";
-                innerText.style.fontWeight="none";
-                innerText.style.textDecoration="line-through";
-        })
+class todoList{
+    constructor(place, title = "to-do list"){
+        this.place = place;
+        this.title = title;
+        this.updateTask = [];
+        this.addingTask();
     }
-});
-const popUpTab1=document.getElementById('popTabForItem');
-function deleteTaskInsideItems(){
-        alert("Am the delete button")
+    addToDo(){
+        let text = this.input.value;
+        this.updateTask.push(new Card(text, this.div, this));//pushing the data for other cards
     }
-function addTaskInsideItems(){
-    popTabForItem.classList.add('active');
-    newScreen.classList.add('active');
-    empty.classList.add('active');
+    addingTask(){
+        this.createToDoListElement();
+        this.place.append(this.parent);
+    }
+    createToDoListElement(){
+        //Creating elements without html
+        this.child = document.createElement('h4');
+        this.child.innerHTML = `${this.title}<hr class="hr">`;
+
+        this.input = document.createElement('input');
+		this.input.classList.add('ashi');
+
+        this.addIcon = document.createElement('i');
+        this.addIcon.setAttribute("class","fa-sharp fa-solid fa-circle-plus");
+        this.addIcon.id = "to-do-list-add-icon";
+
+		this.deleteIcon = document.createElement('i');
+
+		this.deleteIcon.setAttribute("class","fa-solid fa-trash-can");
+		
+        this.deleteIcon.id = "to-do-list-delete-icon";
+        this.div = document.createElement('div');//for body
+
+        this.parent = document.createElement('div');
+		this.parent.setAttribute("id","card-design");
+		this.parent.classList.add("todoList");
+        const inputnew=document.getElementById('box');
+        //Append elements to the to-do list element
+        this.parent.append(this.child);
+        inputnew.append(this.input);
+		this.parent.append(this.deleteIcon);
+        this.parent.append(this.addIcon);
+        this.parent.append(this.div);
+		
+		this.deleteIcon.addEventListener('click', ()=>{
+			alert("Am the a delete button")
+			// this.card.remove();
+		    // let a = this.todoList.updateTask.indexOf(this);
+		    // this.todoList.updateTask.splice(a,1);
+		});
+		const add=document.getElementById('work');
+		this.addIcon.addEventListener('click',()=>{
+			this.input.classList.add('ashi');
+			popTabForItem.classList.add('active');
+			newScreen.classList.add('active');
+			empty.classList.add('active');
+            add.classList.add('add');
+		});
+		const pop=document.getElementsByClassName('btn11');
+        pop[0].addEventListener('click', ()=>{
+			this.input.classList.add('ashika');
+            if(this.input.value != ""){
+                this.addToDo.call(this);
+                this.input.value = "";
+            }
+        });
+    }
 }
+const popTabForItem=document.getElementById('popTabForItem');
+const addItems=document.getElementById('to-do-list-add-icon');
+
 const clickAddBtn=document.getElementsByClassName('btn11');
 const clickCloseBtn=document.getElementsByClassName('btn12');
+
 clickCloseBtn[0].addEventListener('click',()=>{
     popTabForItem.classList.remove('active');
     newScreen.classList.remove('active'); 
     empty.classList.add('active');
+	// this.input.classList.add('ashi');
 })
 clickAddBtn[0].addEventListener('click',()=>{
     popTabForItem.classList.remove('active');
     newScreen.classList.remove('active');
     empty.classList.add('active');
-    newContent();
 });
+//creating the inner text to card by getting data from the class constructor 
+class Card{
+    constructor(text, place, todoList){
+        this.place = place;
+        this.todoList = todoList;
+        this.showDetails = {
+            text: text,
+            updateList: []
+        }
+        this.addingItems();
+    }
+    addingItems(){
+        this.card = document.createElement('div');
+        this.card.classList.add("card");
+		
+        this.p = document.createElement('p');
+		this.p.innerHTML=this.showDetails.text;
+	    this.btn=document.createElement('button');
+		this.btn.setAttribute('id','markbtn');
+		this.btn.innerText="Mark Done";
+		this.btn.addEventListener('click',()=>{
+			this.p.style.color="red";
+		    this.p.style.textAlign="center";
+            this.p.style.textDecoration="line-through";
+            let a = this.todoList.updateTask.indexOf(this);
+            this.todoList.updateTask.splice(a,1);
+			this.btn.classList.add('active');
+		});
+        this.card.appendChild(this.p);
+        this.card.appendChild(this.btn);
+        this.place.append(this.card);
+    }
+}
+//Main platform
+let addTodoListInput = document.getElementById("addTodoListInput");
+let addTodoListButton = document.getElementById("addTodoListButton");
+
+addTodoListButton.addEventListener('click',()=>{
+   if ( addTodoListInput.value != ""){
+    new todoList(container, addTodoListInput.value);
+    addTodoListInput.value = "";
+   }
+});
+
